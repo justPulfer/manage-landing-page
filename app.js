592e32e7d941dc011aedb7befba68fafdc7773d8
Pulfer Dadson <just_pulfer@Trudys-iMac.gateway.sasktel.net>
2023-11-30 08:02:58 -0800

    fixed the carousel effect with swiper js....have to fix position and mobile view

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const expandMenu = document.getElementById("bx");
-const headerNaVLists = document.getElementById("header-nav-lists");
+const headerNavLists = document.getElementById("header-nav-lists");
 // const body = document.body;
 
 expandMenu.addEventListener("click", (e) => {
-	if (headerNaVLists.style.display === "flex") {
-		headerNaVLists.style.display = "none";
+	if (headerNavLists.style.display === "flex") {
+		headerNavLists.style.display = "none";
 		expandMenu.classList.add("bx-menu");
 		expandMenu.classList.remove("bx-x");
 	} else {
-		headerNaVLists.style.display = "flex";
+		headerNavLists.style.display = "flex";
 		expandMenu.classList.add("bx-x");
 		expandMenu.classList.remove("bx-menu");
 		// body.classList.add("shadow");
 	}
+});
+
+// Swiper js
+const swiper = new Swiper(".swiper", {
+	slidesPerView: "3",
+	grabCursor: "true",
+	loop: "true",
+	spaceBetween: "10",
+	centeredSlides: "false",
+	pagination: "true",
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true,
+	},
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
 });
